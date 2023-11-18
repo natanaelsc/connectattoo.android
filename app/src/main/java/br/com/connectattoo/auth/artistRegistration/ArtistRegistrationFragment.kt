c4acdc475fbecaa8522f3f08518b5d2f8003eb58
Natanael Silva <natanael96@live.com>
2023-11-18 10:26:04 -0300

    refactor: remove unused import

--- app/src/main/java/br/com/connectattoo/auth/artistRegistration/ArtistRegistrationFragment.kt
+++ app/src/main/java/br/com/connectattoo/auth/artistRegistration/ArtistRegistrationFragment.kt
@@ -11,7 +11,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import br.com.connectattoo.R
 import br.com.connectattoo.databinding.FragmentArtistRegistrationBinding
