6928f95879f61f0c947dc44c09e43eb3de408141
Felipe Morilho <felipemdecastro@gmail.com>
2023-12-18 09:55:40 -0300

    feat: add ConfirmationFragment class

---
+++ app/src/main/java/br/com/connectattoo/ui/confirmation/ConfirmationFragment.kt
@@ -0,0 +1,33 @@
+package br.com.connectattoo.ui.confirmation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.connectattoo.R
+import br.com.connectattoo.databinding.FragmentConfirmationBinding
+import br.com.connectattoo.ui.BaseFragment
+
+class ConfirmationFragment : BaseFragment<FragmentConfirmationBinding>() {
+
+    override fun setupViews() {
+    }
+
+    override fun inflateBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentConfirmationBinding {
+        return FragmentConfirmationBinding.inflate(inflater, container, false)
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_confirmation, container, false)
+    }
+
+
+}
