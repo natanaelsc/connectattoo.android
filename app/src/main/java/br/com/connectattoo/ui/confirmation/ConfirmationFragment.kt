979fbd1347888e93484423125315c4614ff4ae3e
Felipe Morilho <felipemdecastro@gmail.com>
2024-01-04 11:09:44 -0300

    chore: remove imports not used on ConfiramtionFragment

--- app/src/main/java/br/com/connectattoo/ui/confirmation/ConfirmationFragment.kt
+++ app/src/main/java/br/com/connectattoo/ui/confirmation/ConfirmationFragment.kt
@@ -1,11 +1,8 @@
 package br.com.connectattoo.ui.confirmation
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
+
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import br.com.connectattoo.R
 import br.com.connectattoo.databinding.FragmentConfirmationBinding
 import br.com.connectattoo.ui.BaseFragment
 
