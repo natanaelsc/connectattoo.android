8f885c94db6e71d4aac34303db5eb9df6deb2193
Felipe Morilho <felipemdecastro@gmail.com>
2024-01-15 12:51:50 -0300

    chore: refactoring class

--- app/src/main/java/br/com/connectattoo/api/ApiService.kt
+++ app/src/main/java/br/com/connectattoo/api/ApiService.kt
@@ -1,11 +1,15 @@
 package br.com.connectattoo.api
 
-import br.com.connectattoo.domain.UserDomain
+import br.com.connectattoo.data.TokenData
+import br.com.connectattoo.data.UserData
+import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface ApiService {
 
-    @POST("register")
-    suspend fun registerUser (@Body registrationData: UserDomain)
+    @Headers("Content-Type: application/json")
+    @POST("auth/register")
+    fun registerUser (@Body registrationData: UserData): Call<TokenData>
 }
