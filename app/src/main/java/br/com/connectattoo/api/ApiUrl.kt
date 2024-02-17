46c087b5d8287933aea9068ffc23bbcdc5837009
Natanael Silva <natanael96@live.com>
2024-02-17 09:12:17 -0300

    chore(api): change api base url

--- app/src/main/java/br/com/connectattoo/api/ApiUrl.kt
+++ app/src/main/java/br/com/connectattoo/api/ApiUrl.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiUrl {
-    private const val BASE_URL = "http://192.168.1.6:3000/api/v1/"
+    private const val BASE_URL = "https://connectattoo-api-staging.up.railway.app/api/v1/"
 
     val instance: Retrofit by lazy {
         Retrofit.Builder()
