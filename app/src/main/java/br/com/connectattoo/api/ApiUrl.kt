46a36dad4b0aeb7e049d7b23adcd9ed961860d2b
Natanael Silva <natanael96@live.com>
2024-02-18 19:48:53 -0300

    chore: add build enviroment configuration

--- app/src/main/java/br/com/connectattoo/api/ApiUrl.kt
+++ app/src/main/java/br/com/connectattoo/api/ApiUrl.kt
@@ -1,14 +1,14 @@
 package br.com.connectattoo.api
 
+import br.com.connectattoo.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiUrl {
-    private const val BASE_URL = "https://connectattoo-api-staging.up.railway.app/api/v1/"
 
     val instance: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
