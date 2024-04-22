3d9040ade2411b34fc394a6634cab847376214c0
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2024-04-22 15:01:00 -0400

    fix: refactors occurrences from Customer Profile to Tattoo Client Profile

--- app/src/main/java/br/com/connectattoo/ui/tattooclientprofile/TattooClientProfileState.kt
+++ app/src/main/java/br/com/connectattoo/ui/tattooclientprofile/TattooClientProfileState.kt
@@ -1,17 +1,17 @@
-package br.com.connectattoo.ui.customerprofile
+package br.com.connectattoo.ui.tattooclientprofile
 
 import br.com.connectattoo.data.MyGalleryProfile
-import br.com.connectattoo.data.TagCustomerProfile
+import br.com.connectattoo.data.TagTattooClientProfile
 
-data class CustomerProfileState(
+data class TattooClientProfileState(
     val txtNameUser: String? = null,
     val txtAgeAndEmail: String? = null,
     val txtNameTattooArtist: String? = null,
     val txtTattooArtistProfile: String? = null,
     val txtScheduleTomorrow: String? = null,
     val txtScheduleHour: String? = null,
-    val listTagsCustomerProfile: List<TagCustomerProfile>? = listOf(),
-    val listGalleriesCustomerProfile: List<MyGalleryProfile>? = listOf(),
+    val listTagsTattooClientProfile: List<TagTattooClientProfile>? = listOf(),
+    val listGalleriesTattooClientProfile: List<MyGalleryProfile>? = listOf(),
     val userImage: String? = null,
     val imageTattooArtist: String? = null
 )
