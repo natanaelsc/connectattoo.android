74d450d863da35eef92cd600a33808c89bf348fa
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2024-05-06 11:04:30 -0400

    refactor: remove mock fake galleries list

--- app/src/main/java/br/com/connectattoo/api/response/TattooClientProfileResponse.kt
+++ app/src/main/java/br/com/connectattoo/api/response/TattooClientProfileResponse.kt
@@ -13,7 +13,7 @@ data class TattooClientProfileResponse(
     val imageProfile: String? = "",
     val tags: List<Tag> = emptyList(),
     val email: String? = "",
-    val galleries: List<Gallery> = mock()
+    val galleries: List<Gallery> = emptyList()
 ) {
     fun toTattooClientProfileEntity(): TattooClientProfileEntity {
         return TattooClientProfileEntity(
@@ -26,29 +26,4 @@ data class TattooClientProfileResponse(
             galleries = this.galleries.toGalleryList()
         )
     }
-}
-
-fun mock(): List<Gallery> {
-    val gallery = listOf(
-        Gallery(
-            id = "6524a08f-a04d-4bc2-97f4-c975a329ed29",
-            name = "Animais Cartoon"
-        ),
-        Gallery(
-            "535c2bc1-46b8-47e2-84ee-ac3dd4cead1f",
-            "Caveiras"
-        ),
-        Gallery(
-
-            "5e880889-8093-4b31-82c9-f331ce8fc92e",
-            "Mãos"
-        ),
-        Gallery(
-            "d8c7cf42-184f-4d03-906d-6b4daa8bc72a",
-            "Costas Fechadas"
-        )
-
-
-    )
-    return gallery
 }
