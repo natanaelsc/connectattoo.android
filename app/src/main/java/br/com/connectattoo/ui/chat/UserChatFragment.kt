a332533b20706f14f7fa7dd766cb433088a15c34
FlavioSabino <s.flavio10@hotmial.com>
2024-05-20 17:28:33 -0300

    feat:create navigation for chat and explorar screens

--- app/src/main/java/br/com/connectattoo/ui/chat/UserChatFragment.kt
+++ app/src/main/java/br/com/connectattoo/ui/chat/UserChatFragment.kt
@@ -1,16 +1,11 @@
 package br.com.connectattoo.ui.chat
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
+
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
-import br.com.connectattoo.R
 import br.com.connectattoo.databinding.FragmentUserChatBinding
-import br.com.connectattoo.databinding.FragmentUserSearchBinding
 import br.com.connectattoo.ui.BaseFragment
-import br.com.connectattoo.ui.search.UserSearchViewModel
+
 
 class UserChatFragment : BaseFragment<FragmentUserChatBinding>() {
 
@@ -21,7 +16,7 @@ class UserChatFragment : BaseFragment<FragmentUserChatBinding>() {
         return FragmentUserChatBinding.inflate(inflater, container, false)
     }
 
-    override fun setupViews(){}
+    override fun setupViews() = Unit
 
 
 
